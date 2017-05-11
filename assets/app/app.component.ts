70fc6e6089cb26d3128510899484d8c46d291c11
Mike Davis <mikedavis@Mikes-MacBook-Air.local>
2017-05-11 21:54:22 +0100

    initial commit

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { Message } from "./message.model";
+import { MessageService } from "./message.service";
+
 
 @Component({
     moduleId: module.id,
     selector: 'my-app',
-    templateUrl: 'app.template.html'
+    templateUrl: 'app.template.html',
+    providers: [MessageService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
+    title = 'Angular 2 and Mongo DB';
+
+    employees = [
+        {name: "Mike Davis", position:"manager"},
+        {name: "Tanya", position:"manager"},
+        {name: "Harold", position:"designer"},
+    ];
+
+    constructor(private messageService: MessageService){ }
+
+    messages: Message[] = [];
+
+
+    model:any={};
+    msg:any="";
     
+    addEmployee(){
+
+        const rnd = Math.ceil(Math.random() * 100);
+        const message = new Message('employee  name ',this.model.position );
+        this.messages.push(message);
+        this.messageService.saveMessage(message)
+            .subscribe(
+                () => console.log('Sucess'),
+                error => console.error(error)
+            );
+        console.log('add');
+        this.employees.push(this.model);
+        this.model = {};
+        this.msg = "Successfully added";
+    };
+
+    deleteEmployee(i){
+        console.log(i);
+        this.employees.splice(i,1);
+        this.msg = "Successfully deleted";
+    };
+    employeeValue;
+    editEmployee(k){
+        this.model.name = this.employees[k].name;
+        this.model.position = this.employees[k].position;
+        this.employeeValue = k;
+
+    };
+
+    updateEmployee(){
+        console.log('update');
+        let k = this.employeeValue;
+        let len = this.employees.length;
+        for (let i=0; i<len; i++){
+            if (i===k){
+                this.employees[i] = this.model;
+                this.model = {};
+            }
+        }
+        this.msg = "Successfully updated";
+    };
+
+    closeMsg(){
+        this.msg = "";
+    }
+
+    ngOnInit(){
+    };
 }
--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,44 @@
 var express = require('express');
 var router = express.Router();
+//var mongoose = require('mongoose');
+var Message = require('../models/message');
 
 router.get('/', function(req, res, next) {
     res.render('index');
+});
+
+router.get('/messages',function(req, res, next){
+    Message.find(function(err,result){
+        if (err){
+            //use return so not to execute the rest of the code
+            return res.status(500).json({
+                message:'error fetching messages'
+            });
+        }
+        res.status(200).json({
+            data: result
+        });
+    })
+});
+
+
+router.post('/message', function(req, res, next){
+    console.log('posting message');
+    var message = new Message({
+        content:req.body.content
+    });
+
+    message.save(function(err,result){
+        console.log('save message to mongo db');
+        if (err){
+            return res.status(500).json({
+                message:'error saving'
+            });
+        }
+        res.status(201).json({
+            message:'Data saved'
+        });
+    });
 });
 
 module.exports = router;
